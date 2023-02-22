d1b058ccbd87dcda316c990ab4e5f60ff886f948
Pratik <prshende@mitaoe.ac.in>
2023-02-22 21:11:47 +0530

    First commmit

---
+++ liniar_search.py
@@ -0,0 +1,19 @@
+def linear_search(list,target):
+    for i in range(len(list)):
+        if list[i]==target:
+            return i
+    return None
+
+def verify(index):
+    if index is not None:
+        print("Target found at index",index)
+    else:
+        print("Target not found")
+
+n=[11,2,33,4,5,6,7,8]
+result= linear_search(n,3)
+verify(result)
+
+
+
+
